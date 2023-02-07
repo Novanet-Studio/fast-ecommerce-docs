6e88a840c146c4f4e603ba2259d93d9885f51c36
Fredy Polania <fpolania@gmail.com>
2023-02-06 20:23:26 -0400

    fix: theme and content

---
+++ src/consts.ts
@@ -0,0 +1,51 @@
+export const SITE = {
+  title: "Novanet Studio - Tienda en mano changelog",
+  description: "Changelog aplicación web Tienda en Mano",
+  defaultLanguage: "en-ve",
+} as const;
+
+export const OPEN_GRAPH = {
+  image: {
+    src: "https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true",
+    alt:
+      "astro logo on a starry expanse of space," +
+      " with a purple saturn-like planet floating in the right foreground",
+  },
+  twitter: "novanetstudio",
+};
+
+export const KNOWN_LANGUAGES = {
+  Español: "es",
+} as const;
+export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
+
+export const GITHUB_EDIT_URL = `https://novanet.studio`;
+
+export const COMMUNITY_INVITE_URL = `https://wa.me/message/QA5DVPVFSXYCJ1`;
+
+// See "Algolia" section of the README for more information.
+export const ALGOLIA = {
+  indexName: "XXXXXXXXXX",
+  appId: "XXXXXXXXXX",
+  apiKey: "XXXXXXXXXX",
+};
+
+export type Sidebar = Record<
+  (typeof KNOWN_LANGUAGE_CODES)[number],
+  Record<string, { text: string; link: string }[]>
+>;
+export const SIDEBAR: Sidebar = {
+  es: {
+    "2022": [
+      { text: "v0.1.0", link: "es/2022/v010" },
+      { text: "v0.2.0", link: "es/2022/v020" },
+      { text: "v0.2.1", link: "es/2022/v021" },
+      { text: "v0.3.3", link: "es/2022/v033" },
+      { text: "v0.4.4", link: "es/2022/v044" },
+    ],
+    "2023": [
+      { text: "Page 2", link: "es/page-2" },
+      { text: "Page 3", link: "es/page-3" },
+    ],
+  },
+};
---
+++ src/content/docs/es/bienvenido.md
@@ -0,0 +1,8 @@
+---
+title: "⭐ Bienvenido"
+description: "Introducción al changelog"
+---
+
+**Documentación de Tienda en Mano**
+
+Historial de las versiones y cambios realizados en el desarrollo de la aplicación web tales como registro de modificaciones, correcciones de errores, nuevas funciones, mejoras, actualización y soporte, entre otros.
